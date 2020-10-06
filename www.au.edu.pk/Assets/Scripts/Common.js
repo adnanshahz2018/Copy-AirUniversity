6abd1daf93f408b67b649d7724e383587a40f216
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-10-06 12:58:17 +0500

    Initial commit

---
+++ www.au.edu.pk/Assets/Scripts/Common.js
@@ -0,0 +1,45 @@
+﻿////////////////////////////////////////////////Event Calendar///////////////////////////////////////////////////////////////
+function GetEventCalendar() {
+    var filePath = "AU_Data/Calendar_Events.txt";
+    var date='2017-11-10';
+    $.ajax({
+        type: "POST",
+        url: "UtilityMethods.aspx/GetCalendarEventData",
+        data: '{filePath: "' + filePath + '" }',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            var data = [];
+          
+            for (var i = 0; i < response.d.length; i++) {
+                data.push({
+                    "id": response.d[i].id,
+                    "name": response.d[i].name,
+                    "startdate": response.d[i].startdate,
+                    "enddate": response.d[i].enddate,
+                    "starttime": response.d[i].starttime,
+                    "endtime": response.d[i].endtime,
+                    "color": response.d[i].color,
+                    "url": response.d[i].url
+                });
+            }
+
+            var dataCalendar = {
+                monthly: data
+            };
+
+            $('#mycalendar').monthly({
+                mode: 'event',
+                dataType: 'json',
+                events: dataCalendar
+               
+            });
+
+        },
+        failure: function (response) {
+            console.log(response.d);
+        }
+    });
+}
+///////////////////////////////////////////////////////////////////End Event Calendar Function////////////////////////////////////////////////////////////////////////
+
